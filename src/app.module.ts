fa7398f6615ae86124f335d39c416d76f77e1139
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-10-23 15:45:00 +0200

    feat: add configuration and mongoose modules for environment setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
