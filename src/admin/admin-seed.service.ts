86096ce2d6e2531ea065e85f9993b4582d888a54
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-10-27 10:37:27 +0100

    feat: seed default admin in DB on start server

---
+++ src/admin/admin-seed.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { Admin } from './entities/admin.entity';
+
+@Injectable()
+export class AdminSeedService implements OnModuleInit {
+  private readonly logger = new Logger(AdminSeedService.name);
+
+  constructor(
+    @InjectModel(Admin.name) private readonly adminModel: Model<Admin>,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async onModuleInit() {
+    await this.seedDefaultAdmin();
+  }
+
+  private async seedDefaultAdmin(): Promise<void> {
+    try {
+      const adminCount = await this.adminModel.countDocuments();
+
+      if (adminCount > 0) {
+        this.logger.log('Admin already exists in database. Skipping seed.');
+
+        return;
+      }
+
+      const defaultName = this.configService.get<string>('ADMIN_NAME');
+      const defaultLastName = this.configService.get<string>('ADMIN_LASTNAME');
+      const defaultEmail = this.configService.get<string>('ADMIN_EMAIL');
+      const defaultPassword = this.configService.get<string>('ADMIN_PASSWORD');
+
+      if (
+        !defaultName ||
+        !defaultLastName ||
+        !defaultEmail ||
+        !defaultPassword
+      ) {
+        this.logger.warn(
+          'Any ADMIN info not found in environment variables. Skipping admin seed.',
+        );
+
+        return;
+      }
+
+      const hashedPassword = await bcrypt.hash(defaultPassword, 10);
+
+      await this.adminModel.create({
+        name: defaultName,
+        lastName: defaultLastName,
+        email: defaultEmail,
+        password: hashedPassword,
+      });
+
+      this.logger.log(
+        `Default admin created successfully with email: ${defaultEmail}`,
+      );
+    } catch (error) {
+      this.logger.error('Error seeding default admin:', error);
+    }
+  }
+}
--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
+import { AdminSeedService } from './admin-seed.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { Admin, AdminSchema } from './entities/admin.entity';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
+  ],
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminSeedService, AdminService],
 })
 export class AdminModule {}
