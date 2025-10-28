33b15c09081bf7ab4aae455f0c9ba34d81dfd0ef
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-10-28 10:02:50 +0100

    D - Omit password in UpdateAdminDto

--- src/admin/dto/update-admin.dto.ts
+++ src/admin/dto/update-admin.dto.ts
@@ -1,6 +1,4 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateAdminDto } from './create-admin.dto';
 
-export class UpdateAdminDto extends PartialType(
-  OmitType(CreateAdminDto, ['password'] as const),
-) {}
+export class UpdateAdminDto extends PartialType(CreateAdminDto) {}
