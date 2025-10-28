e9d6a3ba46fe1db34cd88cc425ecf215bf8f483a
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-10-28 10:04:11 +0100

    D - Password in CreateAdminDto

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsEmail,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateAdminDto {
   @IsString()
@@ -15,13 +9,4 @@ export class CreateAdminDto {
 
   @IsEmail()
   email: string;
-
-  @IsString()
-  @MinLength(8)
-  @MaxLength(20)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([A-Za-z\d]|[^ ]){8,20}$/, {
-    message:
-      'Password must have an uppercase, a lowercase, and a number character',
-  })
-  password: string;
 }
