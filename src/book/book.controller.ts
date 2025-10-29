d5cf5bf7e1873167d20b7327ed1b1a2f61dafa60
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-10-29 09:41:28 +0100

    A - Get Books

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -6,26 +6,36 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { MongoIdPipe } from 'src/common/pipes/mongo-id.pipe';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Book } from './entities/book.entity';
+import { PAGINATION } from 'src/common/constants/pagination.constants';
 
 @Controller('book')
 export class BookController {
+  private readonly defaultPage = PAGINATION.DEFAULT_PAGE;
+  private readonly defaultLimit = PAGINATION.DEFAULT_LIMIT;
+
   constructor(private readonly bookService: BookService) {}
 
   @Post()
   @Auth()
-  create(@Body() createBookDto: CreateBookDto) {
+  create(@Body() createBookDto: CreateBookDto): Promise<Book> {
     return this.bookService.create(createBookDto);
   }
 
   @Get()
-  findAll() {
-    return this.bookService.findAll();
+  @Auth()
+  findAll(
+    @Query('page') page = this.defaultPage,
+    @Query('limit') limit = this.defaultLimit,
+  ): Promise<{ data: Book[]; total: number; page: number; limit: number }> {
+    return this.bookService.findAll(+page, +limit);
   }
 
   @Get(':id')
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -5,12 +5,16 @@ import { UpdateBookDto } from './dto/update-book.dto';
 import { Book } from './entities/book.entity';
 import { Model } from 'mongoose';
 import { createErrorResponse } from 'src/common/methods/errors';
+import { PAGINATION } from 'src/common/constants/pagination.constants';
 
 @Injectable()
 export class BookService {
+  private readonly defaultPage = PAGINATION.DEFAULT_PAGE;
+  private readonly defaultLimit = PAGINATION.DEFAULT_LIMIT;
+
   constructor(@InjectModel(Book.name) private bookModel: Model<Book>) {}
 
-  async create(createBookDto: CreateBookDto) {
+  async create(createBookDto: CreateBookDto): Promise<Book> {
     const existingBook = await this.bookModel.findOne({
       isbn: createBookDto.isbn,
     });
@@ -36,8 +40,18 @@ export class BookService {
     }
   }
 
-  findAll() {
-    return `This action returns all book`;
+  async findAll(
+    page: number = this.defaultPage,
+    limit: number = this.defaultLimit,
+  ): Promise<{ data: Book[]; total: number; page: number; limit: number }> {
+    const skip = (page - 1) * limit;
+
+    const [data, total] = await Promise.all([
+      this.bookModel.find({ delete: false }).skip(skip).limit(limit),
+      this.bookModel.countDocuments({ delete: false }),
+    ]);
+
+    return { data, total, page, limit };
   }
 
   findOne(id: string) {
