4abf5a8a1d84347f10b0495a34e78da87510d1a7
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-10-30 12:33:56 +0100

    A - Throttler dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AdminModule } from './admin/admin.module';
 import { MailModule } from './mail/mail.module';
 import { BookModule } from './book/book.module';
 import { ReviewModule } from './review/review.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -19,6 +20,12 @@ import { ReviewModule } from './review/review.module';
         uri: process.env.MONGODB_URI,
       }),
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 20,
+      },
+    ]),
     AuthModule,
     AdminModule,
     MailModule,
