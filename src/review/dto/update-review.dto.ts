40c433c0032837eeaeb2f63f658737b91d8de634
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-10-31 10:09:28 +0100

    U - Verify Reviews in Update Review

--- src/review/dto/update-review.dto.ts
+++ src/review/dto/update-review.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateReviewDto } from './create-review.dto';
+import { IsBoolean } from 'class-validator';
 
-export class UpdateReviewDto extends PartialType(CreateReviewDto) {}
+export class UpdateReviewDto extends PartialType(CreateReviewDto) {
+  @IsBoolean()
+  verified?: boolean;
+}
