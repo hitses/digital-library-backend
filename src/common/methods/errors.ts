7d9bff576a913c86611b00c4e74a19e6af0cb073
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-11-14 19:26:35 +0100

    U - Update Error Response controls ISBN duplicates

--- src/common/methods/errors.ts
+++ src/common/methods/errors.ts
@@ -1,5 +1,6 @@
 import {
   BadRequestException,
+  ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
 
@@ -11,8 +12,14 @@ export const createErrorResponse = (model: string, error: any) => {
 };
 
 export const updateErrorResponse = (model: string, error: any) => {
-  if (error.code === 11000)
+  if (error.code === 11000) {
+    if (error.keyPattern?.isbn)
+      throw new ConflictException(
+        `Book with ISBN ${error.keyValue?.isbn} already exists`,
+      );
+
     throw new BadRequestException(`${model} already exists`);
+  }
 
   throw new InternalServerErrorException(`Error updating ${model}: ${error}`);
 };
